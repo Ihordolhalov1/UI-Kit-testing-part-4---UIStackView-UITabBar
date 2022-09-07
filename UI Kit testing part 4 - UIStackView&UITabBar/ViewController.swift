165c41f1bffa0f3c5cfed5bfd01858bf0e27a3c2
Ihordolhalov1 <servicegambro@gmail.com>
2022-09-07 15:53:55 +0100

    first commit

---
+++ UI Kit testing part 4 - UIStackView&UITabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI Kit testing part 4 - UIStackView&UITabBar
+//
+//  Created by Ihor Dolhalov on 16.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
